ba9a03de30c7eb88a7ba2ee47bcdc0f63c9bbebc
ajulkjose246 <mail.ajulkjose@gmail.com>
2022-07-16 13:27:37 +0530

    M Stream

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $(".fhd_btn").hide();
+    $(".hd_btn").hide();
+    $("#movie_details_sec").hide();
+    $(".download_btn").click(function () {
+        $(".fhd_btn").show();
+        $(".hd_btn").show();
+        $(".download_btn").hide();
+    })
+    for (let i = 0; i < 6; i++) {
+    // $("#new_movies"+i).click(function () {
+    //     $("#home_sec").hide()
+    //     $("#new_mov").hide()
+    //     $("#movie_details_sec").show();
+       
+    // })
+}
+})
